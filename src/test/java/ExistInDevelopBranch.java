38f952f13c896a78ab3e4f86eafd5011bf4b82e8
Nijat Muhtar <nijatmn@gmail.com>
2022-06-04 21:54:17 -0400

    Created new class

---
+++ src/test/java/ExistInDevelopBranch.java
@@ -0,0 +1,8 @@
+public class ExistInDevelopBranch {
+    public static void main(String[] args) {
+        System.out.println("This is a code that exist on");
+        System.out.println("develop branch, which means that");
+        System.out.println("everybody who made this branch");
+        System.out.println("available on their local can see it.");
+    }
+}
