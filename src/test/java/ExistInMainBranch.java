6e66ba590beab1dbab0d9e2a2869ab4921c49a57
Nijat Muhtar <nijatmn@gmail.com>
2022-06-04 21:47:33 -0400

    Initial project structure

---
+++ src/test/java/ExistInMainBranch.java
@@ -0,0 +1,8 @@
+public class ExistInMainBranch {
+
+    public static void main(String[] args) {
+        System.out.println("This is a code that exist on");
+        System.out.println("main branch, which means that");
+        System.out.println("everybody has this code. ");
+    }
+}
